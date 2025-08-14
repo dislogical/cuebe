0ae42a8c734fbcd4b1f8e5e446fb43ccb6b97622
Colden Cullen <colden@coldencullen.com>
2025-08-14 21:10:23 +0000

    feat: add initial go infrastructure

---
+++ cmd/root.go
@@ -0,0 +1,59 @@
+// Copyright © 2025 Colden Cullen
+
+package cmd
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/pterm/pterm"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var logger *slog.Logger
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "cuebe",
+	Short: "A cue-based configuration build system.",
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default is .cuebe.yaml)")
+
+	logger = slog.New(
+		pterm.NewSlogHandler(
+			pterm.DefaultLogger.
+				WithWriter(rootCmd.OutOrStdout()).
+				WithLevel(pterm.LogLevelDebug).
+				WithTime(false)))
+
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Search config in current directory with name ".cuebe.yaml".
+		viper.AddConfigPath(".")
+		viper.SetConfigName(".cuebe.yaml")
+	}
+
+	viper.AutomaticEnv()
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		logger.Debug("Using config file", "file", viper.ConfigFileUsed())
+	} else {
+		logger.Debug("Not using config file", "error", err.Error())
+	}
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
--- devenv.nix
+++ devenv.nix
@@ -9,10 +9,24 @@
     pre-commit
     commitizen
     hawkeye
+    cobra-cli
   ];
 
   languages = {
     go.enable = true;
     cue.enable = true;
+  };
+
+  outputs = {
+    cuebe = pkgs.buildGoModule rec {
+      name = "cuebe";
+      src = ./.;
+
+      vendorHash = lib.fakeHash;
+
+      GOFLAGS = [
+        "-o=${name}"
+      ];
+    };
   };
 }
