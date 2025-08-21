d4d73b7b2172b4d1d849c45e377690ce3fe588a8
Colden Cullen <colden@coldencullen.com>
2025-08-20 17:06:05 -0700

    docs(cmd): add docs generator

---
+++ cmd/cuebe/docs.go
@@ -0,0 +1,27 @@
+// Copyright © 2025 Colden Cullen
+// SPDX-License-Identifier: MIT
+
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
+
+// docsCmd represents the docs command
+var docsCmd = &cobra.Command{
+	Use:    "docs",
+	Short:  "Generates documentation from the command tree",
+	Hidden: true,
+	Run: func(cmd *cobra.Command, args []string) {
+		const docPath = "docs/cmd"
+		cobra.CheckErr(os.MkdirAll(docPath, os.ModePerm))
+		cobra.CheckErr(doc.GenMarkdownTree(rootCmd, docPath))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(docsCmd)
+}
