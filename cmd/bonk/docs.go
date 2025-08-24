dea7eb7355ad4551572367374917e64f3414d349
Colden Cullen <colden@coldencullen.com>
2025-08-24 14:19:23 -0700

    docs: cmd doc generator strips extra newlines

--- cmd/bonk/docs.go
+++ cmd/bonk/docs.go
@@ -6,11 +6,55 @@
 package main
 
 import (
+	"errors"
+	"io"
 	"os"
+	"sync"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
+
+func trimFileNewlines(waitGroup *sync.WaitGroup, root *os.Root, file string) {
+	stat, err := root.Stat(file)
+	cobra.CheckErr(err)
+
+	entryFile, err := root.OpenFile(file, os.O_RDWR, stat.Mode().Perm())
+	cobra.CheckErr(err)
+
+	size, err := entryFile.Seek(-1, io.SeekEnd)
+	cobra.CheckErr(err)
+
+	readBuf := make([]byte, 1)
+	foundNewlines := int64(0)
+
+	for {
+		read, err := entryFile.Read(readBuf)
+		cobra.CheckErr(err)
+		if read != 1 {
+			cobra.CheckErr(errors.New("failed to read byte in file"))
+		}
+		char := readBuf[0]
+
+		if char == '\n' {
+			_, err = entryFile.Seek(-2, io.SeekCurrent)
+			cobra.CheckErr(err)
+			foundNewlines++
+		} else {
+			break
+		}
+	}
+
+	if foundNewlines > 1 {
+		err = entryFile.Truncate(size + 1 - (foundNewlines - 1))
+		cobra.CheckErr(err)
+	}
+
+	err = entryFile.Close()
+	cobra.CheckErr(err)
+
+	waitGroup.Done()
+}
 
 // docsCmd represents the docs command.
 var docsCmd = &cobra.Command{
@@ -21,6 +65,24 @@ var docsCmd = &cobra.Command{
 		const docPath = "docs/cmd"
 		cobra.CheckErr(os.MkdirAll(docPath, 0o750))
 		cobra.CheckErr(doc.GenMarkdownTree(rootCmd, docPath))
+
+		waitGroup := sync.WaitGroup{}
+
+		// Trim extra trailing newlines from each file
+		root, err := os.OpenRoot(docPath)
+		cobra.CheckErr(err)
+		entries, err := os.ReadDir(docPath)
+		cobra.CheckErr(err)
+		for _, entry := range entries {
+			if entry.IsDir() {
+				continue
+			}
+
+			waitGroup.Add(1)
+			go trimFileNewlines(&waitGroup, root, entry.Name())
+		}
+
+		waitGroup.Wait()
 	},
 }
 
